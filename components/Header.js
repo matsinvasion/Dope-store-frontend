519a733510aee6aa06a504f326a8eb54e5880fb6
matsinvasion <matkmusasizi@gmail.com>
2020-05-23 12:42:12 +0300

    Header styling

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
 import Nav from './Nav';
+import { Link } from 'next/link';
+import { styled } from 'styled-components';
 
+const Logo = styled.h1`
+font-size:4rem;
+margin-left:2rem;
+position:relative;
+z-index:2;
+transform:skew(-7);
+a{
+    padding: 0.5rem 1rem;
+    background: ${props => props.theme.red};
+    color:white;
+    text-transform: uppercase;
+    text-decoration:none;
+}
+@media (max-width: 1300px) {
+    margin:0;
+    text-align:center;
+}
+`;
 const Header = () => {
     return ( 
         <div>
             <div className="bar">
-                <a>dopeStore</a>
+                <h1>
+                <Link>
+                <a>
+                dopeStore
+                </a>
+                </Link></h1>
+                
                 <Nav />
             </div>
             <div classname="sub-bar">
