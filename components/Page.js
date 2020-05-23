e345ee8195b87c3395aa3e3a00949ff2c5b3b437
matsinvasion <matkmusasizi@gmail.com>
2020-05-23 12:10:17 +0300

    theme

--- components/Page.js
+++ components/Page.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Meta from './Meta';
+import { black } from '../.next/static/runtime/main';
+
+const theme = {
+    red:'#FF0000',
+    black:'#393939',
+    grey:'#3A3A3A',
+    lightgrey:'#E1E1E1',
+    maxWidth:'1000px',
+    bs:'0 12px 24px 0 rgba(0,0,0,0.09)',
+}
 
  class Page extends Component {
      state = {  }
