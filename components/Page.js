bf7fd8efa5f9594ec783a224c78aba398d1132f1
matsinvasion <matkmusasizi@gmail.com>
2020-05-23 13:42:32 +0300

    typo

--- components/Page.js
+++ components/Page.js
@@ -12,15 +12,36 @@ const theme = {
     bs:'0 12px 24px 0 rgba(0,0,0,0.09)',
 }
 
-const styledPage = style.div`
+const styledPage = styled.div`
  background:white;
  color:${props => props.theme.black}:`;
+
 
  const Inner = styled.div`
  max-width:${props => props.theme.maxwidth};
  margin:0 auto;
  padding:2rem`;
 
+injectGlobal`
+html{
+    box-sizing:border-box;
+    font-size:10px;
+}
+*,*:before,*:after{
+    box-sizing:inherit;
+}
+body{
+    padding:0;
+    margin: 0;
+    font-size:1.5rem;
+    line-height:2;
+    font-family: 'radnika_next';
+}
+a{
+    text-decoration: none;
+    color: ${theme.black};
+}
+`;
  class Page extends Component {
      state = {  }
      render() { 
