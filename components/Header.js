cc9ae345a6329d86f900372fb75e2f761b10b1fe
matsinvasion <matkmusasizi@gmail.com>
2020-05-24 21:18:26 +0300

    proper calling

--- components/Header.js
+++ components/Header.js
@@ -1,53 +1,53 @@
 import React, { Component } from 'react';
 import Nav from './Nav';
-import { Link } from 'next/link';
+import Link from 'next/link';
 import styled from 'styled-components';
 import Router from 'next/router';
 import NProgress from 'nprogress';
 
-onRouteChangeStart = () => {
+Router.onRouteChangeStart = () => {
     NProgress.start();
 }
-onRouteChangeComplete = () => {
+Router.onRouteChangeComplete = () => {
     NProgress.done();
 }
-onRouteChangeError = () => {
+Router.onRouteChangeError = () => {
     NProgress.done();
 }
 
 
 const Logo = styled.h1`
-font-size:4rem;
-margin-left:2rem;
-position:relative;
-z-index:2;
-transform:skew(-7);
-a{
+font-size: 4rem;
+margin-left: 2rem;
+position: relative;
+z-index: 2;
+transform:skew(-7deg);
+a {
     padding: 0.5rem 1rem;
     background: ${props => props.theme.red};
-    color:white;
+    color: white;
     text-transform: uppercase;
-    text-decoration:none;
+    text-decoration: none;
 }
 @media (max-width: 1300px) {
-    margin:0;
-    text-align:center;
+    margin: 0;
+    text-align: center;
 }
 `;
 const StyledHeader = styled.header`
-.bar{
-    border-bottom:10px solid ${props=> props.theme.black};
-    display:grid;
+.bar {
+    border-bottom: 10px solid ${props=> props.theme.black};
+    display: grid;
     grid-template-columns: auto 1fr;
     justify-content: space-between;
-    align-items:stretch;
-    @media (max-width:1300px){
-        grid-template-columns:1fr;
-        justify-content:center;
+    align-items: stretch;
+    @media (max-width: 1300px){
+        grid-template-columns: 1fr;
+        justify-content: center;
     }
 }
-.sub-bar{
-    display:grid;
+.sub-bar {
+    display: grid;
     grid-template-columns: 1fr auto;
     border-bottom: 1px solid ${props => props.theme.lightgrey}
 }
@@ -56,16 +56,15 @@ const Header = () => {
     return ( 
         <StyledHeader>
             <div className="bar">
-                <h1>
-                <Link>
-                <a>
-                dopeStore
-                </a>
-                </Link></h1>
+                <Logo>
+                <Link href="/">
+                <a>Dope Store</a>
+                </Link>
+                </Logo>
                 
                 <Nav />
             </div>
-            <div classname="sub-bar">
+            <div className="sub-bar">
                  <p>Search</p>
             </div>
             <div>
