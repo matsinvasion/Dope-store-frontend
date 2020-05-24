fb0d2496b404a24575a72ce513f078bccb1facbf
matsinvasion <matkmusasizi@gmail.com>
2020-05-24 21:21:40 +0300

    proper declaration

--- components/Page.js
+++ components/Page.js
@@ -5,39 +5,40 @@ import styled,{ ThemeProvider,injectGlobal } from 'styled-components';
 
 const theme = {
     red:'#FF0000',
-    black:'#393939',
-    grey:'#3A3A3A',
-    lightgrey:'#E1E1E1',
-    maxWidth:'1000px',
-    bs:'0 12px 24px 0 rgba(0,0,0,0.09)',
+    black: '#393939',
+    grey: '#3A3A3A',
+    lightgrey: '#E1E1E1',
+    offWhite:'#EDEDED',
+    maxWidth: '1000px',
+    bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
 }
 
-const styledPage = styled.div`
- background:white;
- color:${props => props.theme.black}:`;
+const StyledPage = styled.div`
+ background: white;
+ color:${props => props.theme.black};`;
 
 
  const Inner = styled.div`
- max-width:${props => props.theme.maxwidth};
- margin:0 auto;
- padding:2rem`;
+ max-width: ${props => props.theme.maxWidth};
+ margin: 0 auto;
+ padding: 2rem`;
 
 injectGlobal`
-html{
+html {
     box-sizing:border-box;
     font-size:10px;
 }
 *,*:before,*:after{
     box-sizing:inherit;
 }
-body{
+body {
     padding:0;
     margin: 0;
     font-size:1.5rem;
     line-height:2;
     font-family: 'radnika_next';
 }
-a{
+a {
     text-decoration: none;
     color: ${theme.black};
 }
@@ -46,12 +47,12 @@ a{
      state = {  }
      render() { 
          return ( 
-             <ThemeProvider>
-             <styledPage>
+             <ThemeProvider theme={theme}>
+             <StyledPage>
              <Meta />
              <Header />
                 <Inner>{this.props.children}</Inner>
-             </styledPage>
+             </StyledPage>
              </ThemeProvider>
           );
      }
