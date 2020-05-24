5910d2754fcf765b84213a8b66b425cc56cc46ed
matsinvasion <matkmusasizi@gmail.com>
2020-05-24 21:17:14 +0300

    configuring apollo client

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import App, { container } from 'next/app';
+import { ApolloProvider } from 'react-apollo';
+import withData from '../lib/withData';
 
 import Page from '../components/Page';
 
@@ -7,17 +9,19 @@ class MyApp extends Component {
     state = {  }
     render() { 
         //reference to current visited page
-        const { Component} = this.props;
+        const { Component, apollo } = this.props;
         return (  
             <container>
+                <ApolloProvider client={apollo}>
                 <Page>
                 <Component />
- 
                 </Page>
+                </ApolloProvider>
+               
                
             </container>
         );
     }
 }
  
-export default MyApp ;
+export default withData(MyApp);
