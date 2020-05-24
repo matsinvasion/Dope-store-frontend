6dcd8f86069895c6bc0f686163fcfc222711fd36
matsinvasion <matkmusasizi@gmail.com>
2020-05-24 21:31:53 +0300

    before rendering page/component, fireoff required queries or mutations

--- pages/_app.js
+++ pages/_app.js
@@ -7,9 +7,17 @@ import Page from '../components/Page';
 
 class MyApp extends Component {
     state = {  }
+    static async getInitialProps({Component, ctx}){
+        let pageProps = {};
+        if(Component.getInitialProps){
+            pageProps = await Component.getInitialProps(ctx);
+        }
+        pageProps.query = ctx.query;
+        return { pageProps  }
+    }
     render() { 
         //reference to current visited page
-        const { Component, apollo } = this.props;
+        const { Component, apollo, pageProps } = this.props;
         return (  
             <container>
                 <ApolloProvider client={apollo}>
