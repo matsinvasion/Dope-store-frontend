4da1798ef992ed10fee4f8b369e16cdd36000158
matsinvasion <matkmusasizi@gmail.com>
2020-05-25 11:00:15 +0300

    item component

---
+++ components/item.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Query } from 'react-apollo';
+
+
+class Items extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <p>Items!</p>
+            </div>
+         );
+    }
+}
+ 
+export default Items;
