404a02b5714fdf463db067eb5ddebf207ebf11ad
matsinvasion <matkmusasizi@gmail.com>
2020-05-25 11:05:37 +0300

    query for items

--- components/item.js
+++ components/item.js
@@ -1,5 +1,19 @@
 import React, { Component } from 'react';
 import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+
+//create query, name them same thing as the variable
+const ALL_ITEMS_QUERY=gql`
+query ALL_ITEMS_QUERY{
+    items{
+        id
+        title
+        description
+        image
+        largeI  mage
+    }
+}
+`;
 
 
 class Items extends Component {
