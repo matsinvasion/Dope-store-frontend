4c8b73e66d394ac0994cd0af8e261f105ead9ccf
matsinvasion <matkmusasizi@gmail.com>
2020-05-27 13:30:41 +0300

    re config

---
+++ pages/item.js
@@ -0,0 +1,11 @@
+import SingleItem from '../components/SingleItem';
+
+const Item = props => {
+    return ( 
+        <div>
+            <SingleItem />
+        </div>
+     );
+}
+ 
+export default Item;
