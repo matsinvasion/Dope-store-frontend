d2e6d6450609a71f7ca62cfc3dc9dc3dc276a0c5
matsinvasion <matkmusasizi@gmail.com>
2020-06-08 14:00:32 +0300

    fix endpoint

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444`;
 export const perPage = 4;
-export const prodEndpoint = `https://dope-store-prod-138840cbcd.herokuapp.com/dope-stores-prod/prod`;
+export const prodEndpoint = `https://dope-store-react-prod.herokuapp.com/`;
